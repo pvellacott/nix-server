b0efd11bef5df821d8f253bb04d5bd88a6594f48
Phil Vellacott <philvellacott@proton.me>
2026-08-10 20:23:01 +1200

    add services

--- configuration.nix
+++ configuration.nix
@@ -14,10 +14,11 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  # networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "pooseyhub";
 
   # Configure network connections interactively with nmcli or nmtui.
   networking.networkmanager.enable = true;
+  networking.nameservers = [ "1.1.1.1" "1.0.0.1" ];
 
   # Set your time zone.
   # time.timeZone = "Europe/Amsterdam";
@@ -127,13 +128,151 @@
     };
   };
 
-  networking.firewall.allowedTCPPorts = [ 22 ];
+  # rememba...
+  # /srv/media/movies, /srv/media/tv, /srv/media/music.
+  systemd.tmpfiles.rules = [
+    "d /srv/media 2775 phil media -"
+    "d /srv/media/downloads 2775 phil media -"
+    "d /srv/media/movies 2775 phil media -"
+    "d /srv/media/tv 2775 phil media -"
+  ];
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    settings = {
+      global = {
+        "server role" = "standalone server";
+        "workgroup" = "WORKGROUP";
+        "server string" = "pooseyhub";
+        "netbios name" = "pooseyhub";
+        "security" = "user";
+        "map to guest" = "Bad User";
+        "follow symlinks" = "yes";
+      };
+      media = {
+        "path" = "/srv/media";
+        "browseable" = "yes";
+        "read only" = "no";
+        "valid users" = "phil";
+        "force group" = "media";
+        "create mask" = "0664";
+        "directory mask" = "2775";
+      };
+    };
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.jellyfin = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.sonarr = {
+    enable = true;
+    openFirewall = true;
+    group = "media";
+  };
+
+  services.radarr = {
+    enable = true;
+    openFirewall = true;
+    group = "media";
+  };
+
+  services.prowlarr = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.qbittorrent = {
+    enable = true;
+    openFirewall = true;
+    group = "media";
+    webuiPort = 8080;
+  };
+
+  services.homepage-dashboard = {
+    enable = true;
+    listenPort = 8082;
+    allowedHosts = "*";
+    settings = {
+      title = "Ghil Server";
+      theme = "dark";
+      color = "slate";
+    };
+    services = [
+      {
+        "Media" = [
+          {
+            "Jellyfin" = {
+              href = "http://pooseyhub:8096";
+              description = "Movies, TV, and music";
+              icon = "jellyfin.png";
+            };
+          }
+        ];
+      }
+      {
+        "Downloadurs" = [
+          {
+            "Sonarr" = {
+              href = "http://pooseyhub:8989";
+              description = "TV show automation";
+              icon = "sonarr.png";
+            };
+          }
+          {
+            "Radarr" = {
+              href = "http://pooseyhub:7878";
+              description = "Movie automation";
+              icon = "radarr.png";
+            };
+          }
+          {
+            "Prowlarr" = {
+              href = "http://pooseyhub:9696";
+              description = "Indexer management";
+              icon = "prowlarr.png";
+            };
+          }
+          {
+            "qBittorrent" = {
+              href = "http://pooseyhub:8080";
+              description = "Torrent downloads";
+              icon = "qbittorrent.png";
+            };
+          }
+        ];
+      }
+    ];
+  };
+
+  services.nginx = {
+    enable = true;
+    recommendedProxySettings = true;
+    virtualHosts = {
+      "pooseyhub.home" = {
+        locations."/".proxyPass = "http://127.0.0.1:8082";
+      };
+      "pooseyhub" = {
+        locations."/".proxyPass = "http://127.0.0.1:8082";
+      };
+    };
+  };
+
+  networking.firewall.allowedTCPPorts = [ 22 80 7878 8080 8096 8989 9696 ];
+  users.groups.media = {};
+  users.users.jellyfin.extraGroups = [ "media" ];
   users.users.phil = {
     isNormalUser = true;
-    extraGroups = [ "wheel"];
+    extraGroups = [ "wheel" "media" ];
   };
       
   system.stateVersion = "26.05"; # Did you read the comment?
 
 }
-
