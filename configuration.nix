5f7dab5ddcc678a7a93cc47432cb294dd6b0caab
phil <philvellacott@proton.me>
2026-08-22 09:54:13 +1200

    advertise smb

--- configuration.nix
+++ configuration.nix
@@ -150,6 +150,18 @@ in
     };
   };
 
+  environment.etc."avahi/services/smb.service".text = ''
+    <?xml version="1.0" standalone='no'?>
+    <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+    <service-group>
+      <name replace-wildcards="yes">%h SMB</name>
+      <service>
+        <type>_smb._tcp</type>
+        <port>445</port>
+      </service>
+    </service-group>
+  '';
+
   services.jellyfin = {
     enable = true;
     openFirewall = true;
